537446071075202aa9594aff1cb19b79f27ce0b4
Alexia Velador <alexiavel01@gmail.com>
2022-02-28 17:31:10 -0700

    commit tree 18ed8b1f7a23dde9f2b750e4ac1281d41702da8d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+function writeCards(name, event){
+    let messages = [];
+    for (let i = 0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+// function countDown(){
+//     let countDown = 10;
+//     while (countDown > -1){
+//         console.log(countDown--);
+//     }
+// }
+
+function countDown(a){
+    while(a>=0){
+        console.log(a--);
+    }
+}
+countDown(10);
